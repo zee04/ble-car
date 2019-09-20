5d3f71bcfc4cde3c8b42358ae300fe85044df3fd
zee04 <54157255+zee04@users.noreply.github.com>
2019-09-20 17:58:31 +0530

    Update rc_moto.ino

--- rc_moto.ino
+++ rc_moto.ino
@@ -12,52 +12,57 @@ void setup()
 
 void loop() {
    if (Serial.available())
+   {
   
     bt = Serial.read();
-    Serial.println(bt);
+    
   }
   switch (bt)
   {
     case 'F':
-         m1.setSpeed(100); 
+         m1.setSpeed(255); 
   m1.run(FORWARD); 
-  m2.setSpeed(100);
+  m2.setSpeed(255);
   m2.run(FORWARD);
-  m3.setSpeed(100);
+  m3.setSpeed(255);
  m3.run(FORWARD);
- m4.setSpeed(100);
+ m4.setSpeed(255);
  m4.run(FORWARD);
       break;
+    
     case 'B':
-      m1.setSpeed(100);
+      m1.setSpeed(155);
   m1.run(BACKWARD); 
-  m2.setSpeed(100);
+  m2.setSpeed(155);
   m2.run(BACKWARD);
-  m3.setSpeed(100);
+  m3.setSpeed(155);
   m3.run(BACKWARD); 
-  m4.setSpeed(100);
+  m4.setSpeed(155);
   m4.run(BACKWARD);
       break;
+    
     case 'L':
-       m1.setSpeed(100);
+       m1.setSpeed(155);
   m1.run(BACKWARD); 
-  m2.setSpeed(100); 
+  m2.setSpeed(155); 
   m2.run(BACKWARD);
-m3.setSpeed(100);
+m3.setSpeed(155);
   m3.run(FORWARD);
-  m4.setSpeed(100); 
+  m4.setSpeed(155); 
   m4.run(FORWARD);
       break;
+    
     case 'R':
-      m3.setSpeed(100);
+      m3.setSpeed(155);
   m3.run(BACKWARD); 
-  m4.setSpeed(100); 
+  m4.setSpeed(155); 
   m4.run(BACKWARD);
-m1.setSpeed(100);
+m1.setSpeed(155);
   m1.run(FORWARD); 
-  m2.setSpeed(100); 
+  m2.setSpeed(155); 
   m2.run(FORWARD);
       break;
+    
     case 'G':
        m1.setSpeed(0);
   m1.run(RELEASE); 
@@ -68,6 +73,7 @@ m3.setSpeed(150);
   m4.setSpeed(100); 
   m4.run(FORWARD);
       break;
+    
     case 'I':
       m1.setSpeed(100);
   m1.run(FORWARD); 
@@ -78,6 +84,7 @@ m3.setSpeed(30);
   m4.setSpeed(0); 
   m4.run(RELEASE);
       break;
+    
     case 'H':
          m1.setSpeed(30);
  m1.run(BACKWARD); 
@@ -88,6 +95,7 @@ m3.setSpeed(100);
   m4.setSpeed(150); 
   m4.run(BACKWARD);
       break;
+    
     case 'J':
       m1.setSpeed(150);
   m1.run(BACKWARD); 
@@ -99,6 +107,7 @@ m3.setSpeed(0);
   m4.run(BACKWARD); 
   
       break;
+    
     case 'S':
          m1.setSpeed(0);
    m1.run(RELEASE);
@@ -109,6 +118,16 @@ m3.setSpeed(0);
   m4.setSpeed(0); 
   m4.run(RELEASE);
       break;
+    
+    default:
+         m1.setSpeed(0);
+   m1.run(RELEASE);
+  m2.setSpeed(0); 
+  m2.run(RELEASE);
+m3.setSpeed(0);
+ m3.run(RELEASE);
+  m4.setSpeed(0); 
+  m4.run(RELEASE);
   }
 }
 
